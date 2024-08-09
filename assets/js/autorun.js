7f0fc626da9b6d2ab3c1ae8850528d5b47240619
JASLEEN <125900465+Jas-girl@users.noreply.github.com>
2024-08-09 14:26:42 +0530

    commit

--- assets/js/autorun.js
+++ assets/js/autorun.js
@@ -1,12 +1,48 @@
-function autorun(){
-    $('dice').trigger('click');
-    hold(350).then(function(){
-        for (i = 0; i < 4; i++) {
-        if(current_turn.player[i].current.position+dice_value<=current_turn.player[i].path.length){
-            $(current_turn.player[i].controller).trigger('click');
-            console.log(current_turn.player[i].current.position+dice_value);
-            break;
-         
+var intervalId = "";
+function start_autorun() {
+    intervalId = window.setInterval(function () {
+        if (rank_count > 2) {
+            clearInterval(intervalId);
+            return 0;
         }
- } })
+        if (bot[current_turn.group.toLocaleLowerCase()]) {
+            autorun();
+
+        } else {
+            clearInterval(intervalId);
+
+        }
+    }, hold_time);
+}
+
+
+
+
+function autorun() {
+    // startTimer();
+    if (isPlayerIsMoving) {
+        return 0;
+    }
+
+
+    $("#dice").trigger("click");
+    hold(hold_time).then(() => {
+
+        for (i = 0; i < 4; i++) {
+            if (current_turn.players[i].current_position + dice_value <= current_turn.players[i].path.length) {
+                $(current_turn.players[i].controller).trigger("click");
+                break;
+
+            }
+
+
+        }
+
+
+
+
+
+    });
+
+
 }
